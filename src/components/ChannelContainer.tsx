b9dcde55c63fbcbfbe6abce74b17faeb591ed6b2
theandrunique <theandrunique@gmail.com>
2025-02-22 18:49:36 +0300

    Add prev scroll state recovery in messages history

--- src/components/ChannelContainer.tsx
+++ src/components/ChannelContainer.tsx
@@ -1,16 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import useChannelsStore from "../store/useChannelsStore";
 import useMessagesStore from "../store/useMessagesStore";
+import { Channel } from "../entities";
+
+const ChannelContainerHeader = ({ channel }: { channel: Channel }) => {
+  return (
+    <div className="border-b border-gray-700 p-4">
+      <h2 className="text-xl font-bold text-white">
+        {channel.title || channel.members[0].username}
+      </h2>
+    </div>
+  );
+};
 
 const ChannelContainer = () => {
   const { selectedChannel } = useChannelsStore();
+  const messagesContainerRef = useRef<HTMLDivElement | null>(null);
 
   const { currentMessages, isMessagesLoading, loadMessages, hasMore } =
     useMessagesStore();
 
+  const scrollsPositions = useRef<Record<string, number>>({});
+  const lastChannelId = useRef<string | null>(null);
+
   useEffect(() => {
-    if (selectedChannel !== null) loadMessages(selectedChannel.id);
+    if (selectedChannel === null) return;
+
+    if (lastChannelId.current && messagesContainerRef.current) {
+      scrollsPositions.current[lastChannelId.current] =
+        messagesContainerRef.current.scrollTop;
+    }
+
+    loadMessages(selectedChannel.id);
+    lastChannelId.current = selectedChannel.id;
   }, [selectedChannel]);
+
+  useEffect(() => {
+    if (!selectedChannel || isMessagesLoading || currentMessages.length === 0)
+      return;
+
+    const messagesContainer = messagesContainerRef.current;
+    if (!messagesContainer) return;
+
+    requestAnimationFrame(() => {
+      const savedPosition = lastChannelId.current
+        ? scrollsPositions.current[lastChannelId.current] ||
+          messagesContainer.scrollHeight
+        : messagesContainer.scrollHeight;
+
+      messagesContainer.scrollTo({ top: savedPosition });
+    });
+  }, [currentMessages, isMessagesLoading]);
 
   if (!selectedChannel) {
     return <div>Select a channel to start chatting</div>;
@@ -20,13 +60,12 @@ const ChannelContainer = () => {
 
   return (
     <div className="flex-1 flex flex-col h-full bg-gray-900">
-      <div className="border-b border-gray-700 p-4">
-        <h2 className="text-xl font-bold text-white">
-          {selectedChannel.title || selectedChannel.members[0].username}
-        </h2>
-      </div>
+      <ChannelContainerHeader channel={selectedChannel} />
 
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+      <div
+        className="flex-1 overflow-y-auto p-4 space-y-4"
+        ref={messagesContainerRef}
+      >
         {currentMessages.map((message) => (
           <div key={message.id} className="p-3 bg-gray-800 rounded-lg">
             <p className="text-sm text-gray-300">{message.author.username}</p>
--- src/store/useMessagesStore.tsx
+++ src/store/useMessagesStore.tsx
@@ -4,20 +4,22 @@ import api from "../api/api";
 
 interface MessagesStoreState {
   currentMessages: MessageSchema[];
+  currentChannelId: string | null;
   hasMore: boolean;
   isMessagesLoading: boolean;
   loadMessages: (channelId: string) => Promise<void>;
 }
 
-const limit = 10;
+const limit = 20;
 
 const useMessagesStore = create<MessagesStoreState>((set, get) => ({
   currentMessages: [],
+  currentChannelId: null,
   hasMore: true,
   isMessagesLoading: false,
 
   loadMessages: async (channelId: string) => {
-    set({ isMessagesLoading: true });
+    set({ isMessagesLoading: true, currentChannelId: channelId });
     try {
       const messages = await api.getMessages(channelId, null, limit);
       set({ hasMore: !(messages.length < limit) });
@@ -26,6 +28,25 @@ const useMessagesStore = create<MessagesStoreState>((set, get) => ({
       console.error("Error fetching messages:", err);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+  loadMoreMessages: async () => {
+    const currentChannelId = get().currentChannelId;
+
+    if (currentChannelId === null || !get().hasMore) {
+      return;
+    }
+
+    try {
+      const moreMessages = await api.getMessages(currentChannelId, null, limit);
+
+      const totalMessages = [...get().currentMessages, ...moreMessages];
+
+      set({ hasMore: !(moreMessages.length < limit) });
+
+      set({ currentMessages: totalMessages });
+    } catch (err) {
+      console.error("Error fetching messages:", err);
     }
   },
 }));
