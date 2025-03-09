283c0fecfa9c7db126091c5fd4032319c1f4eb28
theandrunique <theandrunique@gmail.com>
2025-03-09 13:14:52 +0300

    Refactor FullScreenLoading

--- src/components/FullScreenLoading.tsx
+++ src/components/FullScreenLoading.tsx
@@ -1,6 +1,10 @@
 
 const FullScreenLoading = () => {
-  return <div>Loading...</div>
+  return (
+    <div className="bg-[#0e0e10] fixed h-screen w-screen flex flex-col items-center justify-center z-50">
+      <div className="w-12 h-12 animate-spin rounded-full border-4 border-[#efeff1] border-t-[#374151]"></div>
+    </div>
+  )
 }
 
 export default FullScreenLoading;
