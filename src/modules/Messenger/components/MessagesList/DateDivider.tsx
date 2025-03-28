4ca819fc7c765cd6b3fcebae338d0d6b21596cfb
theandrunique <theandrunique@gmail.com>
2025-03-28 17:10:47 +0300

    Add opacity to DateDivider in MessagesList

--- src/modules/Messenger/components/MessagesList/DateDivider.tsx
+++ src/modules/Messenger/components/MessagesList/DateDivider.tsx
@@ -13,7 +13,7 @@ const getDateString = (timestamp: string): string => {
 const DateDivider = ({ timestamp: date }: DateDividerProps) => {
   return (
     <div className="flex w-auto items-center justify-center rounded-full sticky top-0 z-10 pointer-events-none">
-      <div className="px-4 mb-1 py-0.5 bg-[#1f1f23] rounded-full text-white text-sm font-semibold shadow-lg">
+      <div className="px-4 mb-1 py-0.5 bg-[#1f1f23]/80 rounded-full text-white text-sm font-semibold shadow-lg">
         {getDateString(date)}
       </div>
     </div>
