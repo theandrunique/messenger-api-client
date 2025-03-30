baeeb5bfabda5c472383a0532206c6ff2171cef4
theandrunique <theandrunique@gmail.com>
2025-03-30 23:07:33 +0300

    ~

--- src/modules/Messenger/components/ChannelSidebar/ChannelCard.tsx
+++ src/modules/Messenger/components/ChannelSidebar/ChannelCard.tsx
@@ -15,11 +15,11 @@ const ReadStatus = ({
   if (channel.lastMessage?.authorId !== currentUserId) return null;
 
   return (
-    <div className="text-pink-400">
+    <div className="text-fuchsia-400">
       {channel.maxReadAt === channel.lastMessage.id ? (
-        <CheckCheck className="w-4 h-4 text-[#9147ff]" />
+        <CheckCheck className="w-4 h-4" />
       ) : (
-        <Check className="w-4 h-4 text-[#9147ff]" />
+        <Check className="w-4 h-4" />
       )}
     </div>
   );
